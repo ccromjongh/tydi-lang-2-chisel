290945d9d66e8b7a682512f63ddee75201a949f9
Casper Cromjongh <casper.cromjongh@hotmail.com>
2023-06-16 18:46:09 +0200

    Basic ref solver.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from json import loads, dumps
 from jinja2 import Environment, PackageLoader, select_autoescape, FileSystemLoader
 env = Environment(
@@ -11,8 +12,31 @@ def get_json() -> dict:
         return loads(f.read())
 
 
+class LogicType(Enum):
+    stream = 'Stream'
+    bits = 'Bits'
+    group = 'Group'
+    null = 'Null'
+    ref = 'Ref'
+
+
+def pre_process(data: dict, solve: str, l: list[dict]) -> list[dict]:
+    item = data.get(solve)
+    item_type = LogicType(item['type'])
+    if item_type == LogicType.ref:
+        refers_to = item['value']
+        l = pre_process(data, refers_to, l)
+    if not item.get('included', False):
+        l.append(item)
+    return l
+
+
 if __name__ == '__main__':
     tydi_data = get_json()
+    logic_types = tydi_data['logic_types']
+    to_solve = list(logic_types.keys())[-1]
+    processed_data = pre_process(logic_types, to_solve, [])
     template = env.get_template('output.scala')
     output = template.render(tydi_data)
+    print(output)
     pass
--- templates/output.scala
+++ templates/output.scala
@@ -1,3 +1,7 @@
 import tydi_lib._
 import chisel3._
 import chisel3.internal.firrtl.Width
+
+{% for name, type in logic_types.items() %}
+{{ type.type }}: {{ type.get('value', '') }}
+{% endfor %}
