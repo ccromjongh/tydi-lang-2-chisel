0b5edcc4f2533f522d84f70862402ffdc3067176
Casper Cromjongh <casper.cromjongh@hotmail.com>
2024-04-08 14:10:38 +0200

    Add scala-cli directives for direct compilation.

--- tl2chisel/templates/generation_stub.scala
+++ tl2chisel/templates/generation_stub.scala
@@ -1,4 +1,11 @@
-package org.example.{{ compile_options.package_of_top_level_implementation }}
+// scala-cli directives, ignore if using in sbt project
+//> using scala "2.13.12"
+//> using dep "org.chipsalliance::chisel::5.1.0"
+//> using dep "nl.tudelft::tydi-chisel::0.1.0"
+//> using plugin "org.chipsalliance:::chisel-plugin::5.1.0"
+//> using options "-unchecked", "-deprecation", "-language:reflectiveCalls", "-feature", "-Xcheckinit", "-Xfatal-warnings", "-Ywarn-dead-code", "-Ywarn-unused", "-Ymacro-annotations"
+
+package {{ compile_options.package_of_top_level_implementation }}
 
 import {{ compile_options.package_of_top_level_implementation }}.{{ compile_options.top_level_implementation | capitalize }}
 
