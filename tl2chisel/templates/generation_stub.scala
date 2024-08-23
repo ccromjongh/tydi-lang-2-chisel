747d0f90b7d6cdf78a7cbbe230f753d65576a991
Casper Cromjongh <casper.cromjongh@hotmail.com>
2024-08-23 12:11:24 +0200

    Update scala-cli directives for Chisel 6.5.0.
    Change import line to what it says in the Chisel example file.

--- tl2chisel/templates/generation_stub.scala
+++ tl2chisel/templates/generation_stub.scala
@@ -1,16 +1,17 @@
 // scala-cli directives, ignore if using in sbt project
 //> using scala "2.13.12"
-//> using dep "org.chipsalliance::chisel::5.1.0"
+//> using dep "org.chipsalliance::chisel:6.5.0"
+//> using plugin "org.chipsalliance:::chisel-plugin:6.5.0"
+//> using options "-unchecked", "-deprecation", "-language:reflectiveCalls", "-feature", "-Xcheckinit", "-Xfatal-warnings",
 //> using dep "nl.tudelft::tydi-chisel::0.1.0"
-//> using plugin "org.chipsalliance:::chisel-plugin::5.1.0"
-//> using options "-unchecked", "-deprecation", "-language:reflectiveCalls", "-feature", "-Xcheckinit", "-Xfatal-warnings", "-Ywarn-dead-code", "-Ywarn-unused", "-Ymacro-annotations"
 
 package {{ compile_options.package_of_top_level_implementation }}
 
 import {{ compile_options.package_of_top_level_implementation }}.{{ compile_options.top_level_implementation | capitalize }}
 
 import chisel3._
-import circt.stage.ChiselStage
+// _root_ disambiguates from package chisel3.util.circt if user imports chisel3.util._
+import _root_.circt.stage.ChiselStage
 
 import java.io.{File, FileWriter}
 
